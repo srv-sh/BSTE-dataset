6075da9fff039af03edd3750374e13725c366ff7
srv-sh <data.geek.srv@gmail.com>
2023-07-31 18:08:22 +0600

    added all files in one commit

---
+++ cfg.py
@@ -0,0 +1,18 @@
+# dir
+data_dir = '/content/drive/MyDrive/SRNet/SRNet-Datagen/Synthtext/datasets/dataset_for_evaluation'
+i_t_dir = 'i_t'
+i_s_dir = 'i_s'
+t_sk_dir = 't_sk'
+t_t_dir = 't_t'
+t_b_dir = 't_b'
+t_f_dir = 't_f'
+mask_t_dir = 'mask_t'
+g_t_dir = 'g_t'
+
+# sample
+sample_num = 1000
+
+# multiprocess
+process_num = 16
+data_capacity = 256
+
