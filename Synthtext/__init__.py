d26c16bec4ad7937f12b43748d4f57767ab3e270
srv-sh <data.geek.srv@gmail.com>
2023-07-31 21:31:06 +0600

    removed synthtext folder

--- Synthtext/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import data_cfg
-from . import gen
-from . import render_text_mask
-from . import colorize
-from . import poisson_reconstruct
-from . import skeletonization
-from . import render_standard_text
